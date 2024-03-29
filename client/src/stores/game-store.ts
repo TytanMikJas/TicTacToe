3be8f7a61acec3cfd2bea81e1bf4ed35a071d1fc
TytanMikJas <mikjas02@gmail.com>
2024-03-29 20:59:59 +0100

    added important packages

--- client/src/stores/game-store.ts
+++ client/src/stores/game-store.ts
@@ -1,3 +1,28 @@
+// import { Socket } from "socket.io-client";
+// import { createSocket } from "../utils/socket-io";
+// import { create } from "zustand";
+// import { createJSONStorage, persist } from "zustand/middleware";
+
+export interface CreateGameDto {
+  playerId: string;
+  oponentId?: string;
+}
+
+export interface MakeMoveDto {
+  gameId: string;
+  square: number;
+}
+export interface GameStore {
+  pendingGames: Game[];
+  inProgressGames: Game[];
+
+  connect: (userId: string) => void;
+  disconnect: () => void;
+  createGame: (dto: CreateGameDto) => void;
+  acceptGame: (gameId: string) => void;
+  makeMove: (makeMoveDto: MakeMoveDto) => void;
+}
+
 export interface Game {
   id: string;
   player1: string;
