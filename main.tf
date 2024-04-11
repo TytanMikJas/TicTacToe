8b3066f73a4797aacbcf922c2935e49eb69a81f5
Mikołaj Jastrzębski <mikjas02@gmail.com>
2024-04-11 15:04:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
+# Define the AWS provider and specify the region as us-east-1
 provider "aws" {
   region = "us-east-1"
 }
 
+# Create a VPC with the specified CIDR block, enable DNS support and hostnames, and assign tags
 resource "aws_vpc" "app_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -11,6 +13,7 @@ resource "aws_vpc" "app_vpc" {
   }
 }
 
+# Create a subnet within the VPC, specify its CIDR block, availability zone, and assign tags
 resource "aws_subnet" "app_subnet" {
   vpc_id            = aws_vpc.app_vpc.id
   cidr_block        = "10.0.1.0/24"
@@ -20,6 +23,7 @@ resource "aws_subnet" "app_subnet" {
   }
 }
 
+# Create an internet gateway and attach it to the VPC, and assign tags
 resource "aws_internet_gateway" "app_gateway" {
   vpc_id = aws_vpc.app_vpc.id
   tags = {
@@ -27,6 +31,7 @@ resource "aws_internet_gateway" "app_gateway" {
   }
 }
 
+# Create a route table within the VPC, specify a default route to the internet gateway, and assign tags
 resource "aws_route_table" "app_route_table" {
   vpc_id = aws_vpc.app_vpc.id
   route {
@@ -38,16 +43,19 @@ resource "aws_route_table" "app_route_table" {
   }
 }
 
+# Associate the subnet with the route table
 resource "aws_route_table_association" "my_route_table_association" {
   subnet_id      = aws_subnet.app_subnet.id
   route_table_id = aws_route_table.app_route_table.id
 }
 
+# Create a security group to control inbound and outbound traffic, and assign tags
 resource "aws_security_group" "app_sg" {
   name        = "tic_tac_toe_sg"
   description = "Allow web and db traffic"
   vpc_id      = aws_vpc.app_vpc.id
 
+  # Allow SSH, HTTP, and custom ports 3000 and 5000 inbound traffic
   ingress {
     from_port   = 22
     to_port     = 22
@@ -76,6 +84,7 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -88,8 +97,9 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+# Launch an EC2 instance with specified AMI, instance type, subnet, security group, SSH key, assign public IP, and assign tags
 resource "aws_instance" "app_instance" {
-  ami                         = "ami-0a44aefa5a8df82eb" // Przykładowy AMI, zastąp odpowiednim
+  ami                         = "ami-0a44aefa5a8df82eb" // Replace with appropriate AMI
   instance_type               = "t2.small"
   subnet_id                   = aws_subnet.app_subnet.id
   security_groups             = [aws_security_group.app_sg.id]
